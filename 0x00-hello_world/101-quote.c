c7518422651142f63b866df3e31e7e44f1bb16b1
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-16 21:15:31 +0000

    fd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-char messege[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char messege[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 int i;
 for (i = 0 ; messege[i] != '\0'; ++i)
 {
 putchar (messege[i]);
 }
+putchar ('\n');
 return (1);
 }
