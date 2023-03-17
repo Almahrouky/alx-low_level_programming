1b7a639ac6486b91e638e3ae05a7e5d0c8d75a73
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-17 12:36:19 +0000

    df

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 97 ; i <= 122 ; ++i)
+{
+putchar((char)i);
+}
+for (i = 65 ; i < 91 ; ++i)
+{
+putchar((char)i);
+}
+putchar('\n');
+return (0);
+}
