6fcc7ced80c884cb6cffb4c509167956cab267f1
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-17 20:44:58 +0000

    Fd

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int l;
+int k;
+int i;
+int j;
+for (l = 0; l <= 9; ++l)
+{
+for (k = 0; k <= 8; ++k)
+{
+for (i = l; i <= 9; ++i)
+{
+for (j = k ; j <= 9; ++j)
+{
+if (l != i || k != j)
+{
+putchar(l + '0');
+putchar(k + '0');
+putchar(' ');
+putchar(i + '0');
+putchar(j + '0');
+if (l != 9 || k != 8 || i != 9 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
