0a21ae5e738d3124f1f525f26abd135e63dd5372
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-21 20:12:37 +0000

    dk

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - This is a description
+ * @n: This is an integer the user input
+ *
+ * Return: something
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+putchar('-');
+retun (-1);
+}
+else
+{
+putchar('0');
+return (0);
+}
