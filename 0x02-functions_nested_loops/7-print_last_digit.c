5a714bb1e565509bcdd1f183f7d93e5460df80e2
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-21 20:52:14 +0000

    df

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - This is a description
+ * @c: This is an integer the user input
+ *
+ * Return: something
+ */
+int print_last_digit(int c)
+{
+int i;
+i = c % 10;
+return (i);
+}
