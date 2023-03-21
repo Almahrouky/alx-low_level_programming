e298abd37d7e4ea4d34d56320e2f6666f48cfcf8
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-21 18:50:41 +0000

    fd

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,2 @@
-#include <stdio.h>
 #pragma once
-
-/**
- * print_alphabet - print_alphabet
- *
- * Return: Thealphabet.
- */
-int print_alphabet(void)
-{
-int i;
-for (i = 97; i < 123; ++i)
-{
-putchar(i);
-}
-putchar('\n');
-return (0);
-}
+void print_alphabet(void);
