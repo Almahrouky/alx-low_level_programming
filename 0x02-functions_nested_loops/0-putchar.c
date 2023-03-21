49d2af37e7405bb47d218cbdebfbac618e10419f
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-21 03:22:29 +0000

    fd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a[9] = "_putchar";
+int i;
+for (i = 0; i < 8; ++i)
+{
+putchar(a[i]);
+}
+return (0);
+}
