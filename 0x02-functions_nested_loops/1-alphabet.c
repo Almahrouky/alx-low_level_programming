3d4fb75d6c7706a7b07c0b5f6898091e7eba48dd
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-21 12:16:42 +0000

    kdf

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,6 @@
-#include <stdio.h>
 #include "main.h"
 
-/**
- * print_alphabet - print_alphabet
- *
- * Return: Thealphabet.
- */
-int print_alphabet(void)
-{
-int i;
-for (i = 97; i < 123; ++i)
-{
-putchar(i);
-}
-putchar('\n');
-return (0);
-}
-
-/**
+/** 
  * main - check the code
  *
  * Return: Always 0.
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,18 @@
+#include <stdio.h>
 #pragma once
-int print_alphabet(void);
+
+/**
+ * print_alphabet - print_alphabet
+ *
+ * Return: Thealphabet.
+ */
+int print_alphabet(void)
+{
+int i;
+for (i = 97; i < 123; ++i)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
