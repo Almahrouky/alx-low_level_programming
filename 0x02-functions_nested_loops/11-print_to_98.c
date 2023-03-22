96a4b8be545d275ac5cdc73c89466e30cca60c0d
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-22 16:59:37 +0000

    fdk

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,84 @@
+#include "main.h"
+
+/**
+ * print_to_98 - This is a description
+ *@n: is a variable
+ * Return: no someting
+ */
+void print_to_98(int n)
+{
+if (n < 0)
+{
+while(n  !=  0)
+{
+if(n <= -10)
+{
+putchar('-');
+n = n * (-1);
+putchar(n / 10 + '0');
+putchar(n % 10 +'0');
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar('-');
+n = n * -1;
+putchar(n + '0');
+putchar(',');
+putchar(' ');
+}
+n -= 1;
+n = n * (-1);
+}
+}
+if (n < 98)
+{
+while (n  != 99)
+{
+ if (n >= 10)
+{
+putchar(n / 10 + '0');
+putchar(n % 10 +'0');
+if (n != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+else
+{
+putchar(n + '0');
+putchar(',');
+putchar(' ');
+}
+n += 1;
+}
+}
+else
+{
+while(n != 97)
+{
+if (n >= 100)
+{
+putchar(n / 100 + '0');
+int x = n - 100;
+putchar(x / 10 + '0');
+putchar(x % 10 +'0');
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar(n / 10 + '0');
+putchar(n % 10 +'0');
+if (n != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+n -= 1;
+}
+}
+}
