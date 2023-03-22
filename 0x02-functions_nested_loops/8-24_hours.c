b2f0d0c549abb7b586f43850740ab92781bcfc71
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-22 14:55:07 +0000

    dj

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,14 +7,14 @@
  */
 void jack_bauer(void)
 {
-int i,j,k,l;
-for (i = 0; i<= 2; ++i)
+int i, j, k, l;
+for (i = 0; i <= 2; ++i)
 {
-for (j = 0; j<= 9; ++j)
+for (j = 0; j <= 9; ++j)
 {
-for (k = 0; k<= 5; ++k)
+for (k = 0; k <= 5; ++k)
 {
-for (l = 0; l<= 9; ++l)
+for (l = 0; l <= 9; ++l)
 {
 putchar(i + '0');
 putchar(j + '0');
@@ -22,8 +22,8 @@ putchar(':');
 putchar(k + '0');
 putchar(l + '0');
 putchar('\n');
-if (i == 2 && j == 3 && k == 5 && l ==9)
-return (0);
+if (i == 2 && j == 3 && k == 5 && l == 9)
+i = j = k = l = 10;
 }
 }
 }
