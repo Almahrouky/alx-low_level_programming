77d77851279e9c7241b1c2da8da47f0d85ddfbce
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-23 21:26:24 +0000

    fd

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - is a functon
+ *
+ * @n: is a integer
+ * Return: void
+ */
+void print_line(int n)
+{
+if (n > 0)
+{
+while (n--)
+{
+putchar('_');
+}
+}
+putchar('\n');
+}
