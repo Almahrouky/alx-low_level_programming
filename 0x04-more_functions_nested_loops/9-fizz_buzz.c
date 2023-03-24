48cb6b75642343c43c4604fb4cf05a2388128005
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-24 12:36:27 +0000

    fk

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+/**
+ * printfizz - is a functon
+ * Return: void
+ */
+void printfizz(void)
+{
+putchar('F');
+putchar('i');
+putchar('z');
+putchar('z');
+putchar(' ');
+}
+/**
+ * printbuzz - is a function
+ * Return: void
+ */
+void printbuzz(void)
+{
+putchar('B');
+putchar('u');
+putchar('z');
+putchar('z');
+putchar(' ');
+}
+/**
+ * main - is the main function
+ * Return: 0 if it is good otherwise 1
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; ++i)
+{
+if (i < 10)
+{
+if (i % 3 == 0 && i % 5 != 0)
+printfizz();
+else if (i % 5 == 0 && i % 3 != 0)
+printbuzz();
+else
+{
+putchar(i + '0');
+putchar(' ');
+}
+}
+
+else
+{
+if (i % 3 == 0 && i % 5 != 0)
+printfizz();
+else if (i % 5 == 0 && i % 3 != 0)
+printbuzz();
+else if (i % 3 == 0 && i % 5 == 0)
+{
+putchar('F');
+putchar('i');
+putchar('z');
+putchar('z');
+printbuzz();
+}
+else
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
