0932532dbe9974a0254c1f121a00b630da57f26d
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-24 13:03:26 +0000

    fk

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,16 +1,5 @@
-#include <unistd.h>
+#include <stdio.h>
 
-/**
- * putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 /**
  * printfizz - is a functon
  * Return: void
@@ -23,6 +12,7 @@ putchar('z');
 putchar('z');
 putchar(' ');
 }
+
 /**
  * printbuzz - is a function
  * Return: void
