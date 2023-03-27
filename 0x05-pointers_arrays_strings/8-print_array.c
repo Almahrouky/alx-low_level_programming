189e3b668c4414363e87e09eaaa4b68bd680c8d8
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-27 22:50:37 +0000

    kd

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,16 +1,17 @@
 #include "main.h"
- /**
-  * print_array - is a funciton
-  * @a: is an integer
-  * @n: is a pointer
-  * Return: void
-  */
+
+/**
+ * print_array - is a funciton
+ * @a: is an integer
+ * @n: is a pointer
+ * Return: void
+ */
 void print_array(int *a, int n)
 {
 int i;
 for (i = 0; i < n ; ++i)
 {
-printf("%d",a[i]);
+printf("%d", a[i]);
 if (i != n - 1)
 {
 putchar(',');
