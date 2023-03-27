06dfa64db0847aa214f5dc99fbd13c33b349f309
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-27 21:54:50 +0000

    dk

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,17 @@
-#
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - is a function
+ * @s: is a char pointer
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i, j;
+j = strlen(s);
+for (i = j; i > 0; --i)
+{
+putchar(s[i]);
+}
+}
