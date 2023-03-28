d65363e7c59aa77df7797ed72dcb36a7f961d6f3
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-28 12:01:31 +0000

    dk

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+ * main - is the main function
+ *
+ * Return: 0 or 1
+ */
+int main(void)
+{
+int i, random;
+srand((unsigned int)(time(NULL)));
+char numbers[] = "0123456789";
+char letters[] = "abcdefghijklmnopqrstuvwxyz";
+char LETTERS[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char symbols[] = "!@#$%^&*?";
+char password[15];
+random = rand() % 4;
+for (i = 0; i < 14; ++i)
+{
+if (random == 1)
+{
+password[i] = numbers[rand() % 10];
+random = rand() % 4;
+printf("%c", password[i]);
+}
+else if (random == 2)
+{
+password[i] = letters[rand() % 26];
+random = rand() % 4;
+printf("%c", password[i]);
+}
+else if (random == 3)
+{
+password[i] = LETTERS[rand() % 26];
+random = rand() % 4;
+printf("%c", password[i]);
+}
+else
+{
+password[i] = symbols[rand() % 9];
+random = rand() % 4;
+printf("%c", password[i]);
+}
+}
+}
