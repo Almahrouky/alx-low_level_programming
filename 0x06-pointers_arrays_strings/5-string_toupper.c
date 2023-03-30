acc33412cffdf643a3d511bd33bd38c470e55379
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-30 21:31:56 +0000

    dk

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,10 @@
 #include "main.h"
- /**
-  * string_toupper - is a function
-  * @s: is a char pointer
-  * Return: pointer char
-  */
+
+/**
+ * string_toupper - is a function
+ * @s: is a char pointer
+ * Return: pointer char
+ */
 char *string_toupper(char *s)
 {
 return (toupper(s));
