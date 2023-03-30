6bf430214ad42217a06ad937443f9479e49f1347
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-30 22:07:59 +0000

    dk

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * cap_string - is a function
+ * @s: is a pointer char
+ * Return: pointer char
+ */
+char *cap_string(char *s)
+{
+int i;
+while (s[i])
+{
+if (i == 0 || isspace(s[i - 1]) || s[i - 1] == '.')
+s[i] == toupper(s[i]);
+i++;
+}
+return (s);
+}
