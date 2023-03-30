270099115915c7d8c0755dc2b795784156ebe7ff
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-30 20:44:14 +0000

    dk

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strncpy - is a function
+ * @dest: is a pointer char
+ * @src: is a pointer char
+ * @n: is an integer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+return (strncpy(dest, src, n));
+}
