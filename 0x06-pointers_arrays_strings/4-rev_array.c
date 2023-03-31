37c52cf0cbb828646d9e7856964963b74d803ef0
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-31 12:50:13 +0000

    dk

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,12 @@
 void reverse_array(int *a, int n)
 {
 int x, i;
-int b[n];
-x = n - 1;
+int arr[n];
 for (i = 0; i < n; ++i)
 b[i] = a[i];
 for (i = 0; i < n; ++i)
 {
-a[i] = b[x];
 x--;
+a[i] = b[x];
 }
 }
