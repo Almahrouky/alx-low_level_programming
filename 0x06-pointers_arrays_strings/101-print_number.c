10f266408391576b31b7adb7431672854a4ea02a
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-31 14:18:47 +0000

    dj

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * print_number - is a function
- * @n: is an integer
- * Return: void
+ * _strcmp - is a function
+ * @s1: is a pointer char
+ * @s2: is a pointer char
+ * Return: integer
  */
-void print_number(int n)
+int _strcmp(char *s1, char *s2)
 {
+return (strcmp(s1, s2));
 }
