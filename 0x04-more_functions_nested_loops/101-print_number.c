874ecb2b6632c9a8c2b44518b8d1076a2e08c232
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-03-31 13:58:44 +0000

    d

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - is a function
+ * @n: is an integer
+ * Return: void
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+putchar('-');
+n = -n;
+}
+if (n / 10)
+{
+print_number(n / 10);
+}
+putchar(n % 10 + '0');
+}
