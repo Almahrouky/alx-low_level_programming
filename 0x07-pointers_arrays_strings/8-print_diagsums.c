4f6809f1204d45728d14e12e4a81ccc2ffac910f
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-03 15:14:51 +0000

    djk

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagsums - is a function
+ * @a: is a pointer char
+ * @size: is an integer
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+int i, j, sum1, sum2;
+sum1 = 0;
+sum2 = 0;
+for (i = 0; i < size; ++i)
+{
+for (j = 0; j <size; ++j)
+{
+if (i == j)
+sum1 += a[i][j];
+if (i + j == size - 1)
+sum2 += a[i][j];
+}
+}
+printf("%d", sum1);
+putchar('\n');
+pritnf("%d", sum2);
+}
