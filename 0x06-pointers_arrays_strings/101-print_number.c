d61cdaa6819538e186e40680a86e9368f89b1614
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-03 21:21:55 +0000

    d

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,26 +7,5 @@
  */
 void print_number(int n)
 {
-int i, x;
-int a[20];
-x = 0;
-if (n == 0)
-putchar('0');
-else
-{
-if (n < 0)
-{
-putchar('-');
-n = -n;
-}
-while (n > 0)
-{
-a[x++] = n % 10;
-n = n / 10;
-}
-for (i = x - 1; i >= 0; --i)
-{
-putchar(a[i] + '0');
-}
-}
+printf("%d", n);
 }
