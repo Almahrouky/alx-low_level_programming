689979bdfd92a4ca6d4df910ece87fed87b299ad
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-03 14:54:22 +0000

    dk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-void print_chessboard(char s(*a)[8]);
+void print_chessboard(char (*a)[8]);
+unsigned int _strspn(char *s, char *accept);
