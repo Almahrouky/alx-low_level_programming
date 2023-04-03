2a308bfaec72aabd4a6ae47db051fc7e8a641e2f
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-03 12:28:20 +0000

    h

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strchr - is afunction
+ * @s: is a pointer char
+ * @c: is a char
+ * Return: pointer char
+ */
+char *_strchr(char *s, char c)
+{
+strchr(s, c);
+return (s[0]);
+}
