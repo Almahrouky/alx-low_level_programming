ceb7d861a2c133c43cb65b4214732f0b80c23ab6
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-05 04:43:19 +0000

    dk

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - is a function
+ * @s: is a pointer char
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+puts(s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void _puts_recursion(char *s);
