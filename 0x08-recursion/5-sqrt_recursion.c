27bbe6aff6134cc3c78b3f915de7267b6d561610
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-05 14:54:05 +0000

    dk

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,41 @@
 #include "main.h"
 
 /**
+ * sqr - is a function
+ * @n: is a folat
+ * @x: is a float
+ * Return: a float
+ */
+float sqr(float n, float x)
+{
+float res;
+res = (x + n / x) / 2;
+if (x == res)
+return (x);
+else
+{
+sqr(n, res);
+}
+}
+
+/**
  * _sqrt_recursion - is a function
  * @n: is an integer
  * Return: an integer
  */
 int _sqrt_recursion(int n)
 {
-float f;
-int i;
-f = sqrt (n);
-i = sqrt (n);
 if (n < 0)
-return (-1);
-else if (f - i > 0)
 return (-1);
 else
 {
-return (sqrt(n));
+int i;
+i = sqr(n, n);
+if (sqr(n, n) > i)
+return (-1);
+else
+{
+return (sqr(n, n));
+}
 }
 }
