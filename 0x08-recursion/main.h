e0fdc8d95c8c2b66aac73f11baf14cfd3f282af1
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-05 11:40:52 +0000

    di

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -9,5 +11,4 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int _pow(int x, int y);
 int is_palindrome(char *s);
