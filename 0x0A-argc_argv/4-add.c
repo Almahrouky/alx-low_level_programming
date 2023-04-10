81ccf834bd0db2a98b364d73e4a3231ab4fcf525
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-10 09:56:40 +0000

    d

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * main - is the main function
+ * @argc: is an integer
+ * @argv: is an integer
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int flag, i, sum;
+sum = 0;
+flag = 1;
+if (argc == 1)
+;
+else
+{
+for (i = 0; i < argc; ++i)
+{
+if (isdigit(strtol(argv[i], NULL, 10)))
+{
+printf("Error\n");
+flag = 0;
+break;
+}
+}
+}
+if (flag == 0)
+return (1);
+else
+{
+for (i = 0; i < argc; ++i)
+{
+sum += strtol(argv[i], NULL, 10);
+}
+printf("%d\n", sum);
+return (0);
+}
+}
