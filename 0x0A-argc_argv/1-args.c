b2b63dc4ef43f846cc35763d1f210cf80b6cff75
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-10 07:20:49 +0000

    dk

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,10 +4,10 @@
  * main - is the main function
  * @argc: is an integer
  * @argv: is is a pointer char
- * Return: an integer
+ * Return: 0
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-printf("%d \n", argc);
+printf("%d \n", argc - 1);
 return (0);
 }
