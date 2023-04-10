b4a4e8debd5ba35342713e651446336bba863d6f
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-10 08:47:38 +0000

    d

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,6 +8,13 @@
  */
 int main(int argc, char *argv[])
 {
+if (argc != 3)
+{
+printf("ERROR\n");
+return (1);
+}
+else
+{
 int i;
 int mul;
 mul = 1;
@@ -15,4 +22,5 @@ for (i = 1; i < argc; ++i)
 mul *= strtol(argv[i], NULL, 10);
 printf("%d\n", mul);
 return (0);
+}
 }
