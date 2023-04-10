02027a53afa8bc3d74c020ef2a014eb3b3f6f41b
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-10 05:08:12 +0000

    d

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: is is a pointer char
  * Return: an integer
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 printf("%d \n", argc);
 return (0);
