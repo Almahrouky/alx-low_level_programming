bda0c7048d83ea609719cbfa3fb2b33d2e21bff3
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-10 08:28:28 +0000

    djk

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - is the main function
+ * @argc: is an integer
+ * @argv: is an integer
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+int i;
+for (i = 0; i < argc; ++i)
+printf("%s\n", argv[i]);
+return (0);
+}
