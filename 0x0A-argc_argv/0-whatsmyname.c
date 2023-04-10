e6bd0eab9e86a66ac06b6d31a6546fe443cef115
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-10 04:34:58 +0000

    dk

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - is the main function
+ * @argc: is an integer
+ * @argv: is a pointer char
+ * Return: an integer
+ */
+int main(int argc, char *argv[])
+{
+printf("%s \n", argv[0]);
+return (0);
+}
