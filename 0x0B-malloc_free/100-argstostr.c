c2ce4804a818f3e06092cbc27e6fc9d1017360f7
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-11 21:46:04 +0000

    dk

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * argstostr - is the function
+ * @ac: is an integer
+ * @av: is a pointer char
+ * Return: a pointer char
+ */
+char *argstostr(int ac, char **av)
+{
+int i;
+char *ptr = (char *)malloc(2 * ac * sizeof(char));
+if (ptr == NULL)
+return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0, j = 0; i < ac; j++, i = i + 1)
+{
+ptr[i] = av[j];
+ptr[i + 1] = '\n';
+}
+return (ptr);
+}
