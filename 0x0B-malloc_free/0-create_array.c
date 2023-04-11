845322713e8ccd484d74b70462cc960c58802eda
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-11 09:55:31 +0000

    d

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * create_array - is a pionter char
+ * @size: is an unsigned int
+ * @c: is a char
+ * Return: a pointer char
+ */
+char *create_array(unsigned int size, char c)
+{
+int i;
+char *x = malloc(size);
+for (i = 0; i < size; ++i)
+{
+x[i] = c;
+}
+if (size == 0)
+return (NULL);
+else
+return (x);
+}
