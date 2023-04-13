eac4a1280e976918028bf6f7abe6f80e5d2a6662
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-13 20:29:27 +0000

    sdk

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _calloc - is a function
+ * @nmemb: is an unsigned int
+ * @size: is an unsigned int
+ * Return: a pointer char
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+int i;
+int *ptr = malloc(nmemb * size);
+if (ptr == NULL || size == 0 || nmemb == 0)
+return (NULL);
+for (i = 0; i < nmemb; i++)
+ptr[i] = 0;
+return (ptr);
+}
