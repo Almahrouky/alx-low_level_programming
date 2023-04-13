73c20daac963c878bd6eee621bd68d83358405e9
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-13 19:44:31 +0000

    dk

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * malloc_checked - is a function
+ * @b: is an unsigned int
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+b = malloc(sizeof(int));
+if (b == NULL)
+exit(98);
+}
