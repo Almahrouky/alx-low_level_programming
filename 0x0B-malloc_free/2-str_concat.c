a007b5e4f35b1125893adb1a28c5618b5d3d192b
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-14 04:04:01 +0000

    dk

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,15 +8,9 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-unsigned int j, i;
-char *x = (char *)malloc(strlen(s1) + strlen(s2) + 1);
-for (i = 0; i < strlen(s1); ++i)
-x[i] = s1[i];
-j = 0;
-for (; i < strlen(s1) + strlen(s2) + 1; ++i)
-{
-x[i] = s2[j];
-j++;
-}
-return (x);
+char *ptr;
+ptr = malloc(strlen(s1) + strlen(s2) + 1);
+strcpy(ptr, s1);
+strcat(ptr, s2);
+return (ptr);
 }
