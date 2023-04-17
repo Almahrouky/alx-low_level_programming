27eebb2ffe7d74acb3524d2466e755098016558f
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-17 16:01:18 -0700

    dj

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,12 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d == NULL)
-;
-else
+if (d)
 {
-d->name = *name;
+d->name = name;
 d->age = age;
-d->owner = *owner;
+d->owner = owner;
 }
 }
