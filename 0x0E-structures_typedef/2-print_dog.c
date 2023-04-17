e93bceb774790e7995ce2b11eac0d583115ee1b5
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-17 16:24:25 -0700

    dj

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,16 +9,8 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-if (name == NULL)
-printf("Name: (nil)");
-if (age == NULL)
-printf("age: (nil)");
-if (owner == NULL)
-printf("owner: (nil)");
-d->name = name;
-d->age = age;
-d->owner = owner;
+printf("name: %s\n", (d->name) ? d->name : (nil));
+printf("age: %f\n", (d->age) ? d->age : (nil));
+printf("owner: %s\n", (d->owner) ? d->owner : (nil));
 }
-else
-printf("");
 }
