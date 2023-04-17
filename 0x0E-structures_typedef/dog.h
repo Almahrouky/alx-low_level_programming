528993898dd00bc729db381e3b1ce0ee758cc676
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-17 15:26:25 +0000

    DK

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dof - is a struct
+ * @name: is a pointer char
+ * @age: ia an float
+ * #owner: is a pointer char
+ * Return: no return
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+#endif
