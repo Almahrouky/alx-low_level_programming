04d7027112e68054e772f9b578fd6e6bd3872c59
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-17 16:45:20 -0700

    dj

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-typedef struct dog
-{
-char *name;
-float age;
-char *owner;
-}dog_t;
 /**
  * struct dog - is a struct
  * @name: is a pointer char
@@ -13,12 +7,12 @@ char *owner;
  * @owner: is a pointer char
  * Description: some thing
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
