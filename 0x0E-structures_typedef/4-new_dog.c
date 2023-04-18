2b8ecd1d395f56efa1c0abf5cc31a9d718ffdbcf
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-18 11:53:30 +0000

    d

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,79 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- *
+ * _strcpy - is a functio
+ * @des: is some thing
+ * @src: is some thing
+ * Return: some thing
+ */
+char *_strcpy(char *des, char *src)
+{
+int i;
+for (i = 0; src[i]; ++i)
+{
+des[i] = src[i];
+}
+des[i] = '\0';
+return (des);
+}
+
+/**
+ * _strlen - is a function
+ * @s: is a pointer char
+ * Return: int
+ */
+int _strlen(char *s)
+{
+int len, i;
+len = 0;
+for (i = 0; s[i]; ++i)
+{
+len++;
+}
+return (len);
+}
+
+/**
+ * new_dog - is a function
+ * @name: is the naem
+ * @age: ia age
+ * @owner: is owner
+ * Return: pointer
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-struct new_dog
+if (!name || age < 0 || !owner)
+return (NULL);
+dog_t dog;
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
 {
-
-};
-
+return (NULL);
+}
+dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+dog->age = malloc(sizeof(float) * sizeof(age));
+if (dog->age == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog->age);
+free(dog);
+return (NULL);
+}
+dog->name = _strcpy(dog->name, name);
+dog->age = age;
+dog->owner = _strcpy(dog->owner, owner);
+return (dog);
 }
