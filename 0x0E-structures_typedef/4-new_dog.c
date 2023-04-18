72a75677e186e2c24749148c4c04255504072a94
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-18 12:33:08 +0000

    dk

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,6 +46,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 dog_t *dog;
 if (!name || age < 0 || !owner)
 return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
 dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 if (dog->name == NULL)
 {
