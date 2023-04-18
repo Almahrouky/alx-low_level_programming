084ab76b889a38ed63a69614047ffdb4649e1ce4
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-18 12:12:07 +0000

    dk

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,22 +19,6 @@ return (des);
 }
 
 /**
- * _strlen - is a function
- * @s: is a pointer char
- * Return: int
- */
-int _strlen(char *s)
-{
-int len, i;
-len = 0;
-for (i = 0; s[i]; ++i)
-{
-len++;
-}
-return (len);
-}
-
-/**
  * new_dog - is a function
  * @name: is the naem
  * @age: ia age
@@ -46,32 +30,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 dog_t *dog;
 if (!name || age < 0 || !owner)
 return (NULL);
-dog = malloc(sizeof(dog_t));
-if (dog == NULL)
-{
-return (NULL);
-}
-dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
-if (dog->name == NULL)
-{
-free(dog);
-return (NULL);
-}
-dog->age = malloc(sizeof(float));
-if (dog->age == NULL)
-{
-free(dog->name);
-free(dog);
-return (NULL);
-}
-dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-if (dog->owner == NULL)
-{
-free(dog->name);
-free(dog->age);
-free(dog);
-return (NULL);
-}
 dog->name = _strcpy(dog->name, name);
 dog->age = age;
 dog->owner = _strcpy(dog->owner, owner);
