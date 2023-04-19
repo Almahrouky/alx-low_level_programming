79fcb8cf34ad4a3c04a5d06cc69436a6fa114cec
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-19 21:02:59 +0000

    dk

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -7,17 +7,19 @@
  * Return: int
  */
 int main(int argc, char *argv[]) {
+    unsigned char *ptr;
+	int i, num_bytes;
     if (argc != 2) {
         printf("Error\n");
         return 1;
     }
-    int num_bytes = atoi(argv[1]);
+    num_bytes = atoi(argv[1]);
     if (num_bytes < 0) {
         printf("Error\n");
         return 2;
     }
-    unsigned char *ptr = (unsigned char *)main;
-    for (int i = 0; i < num_bytes; i++) {
+    *ptr = (unsigned char *)main;
+    for (i = 0; i < num_bytes; i++) {
         printf("%02x", *(ptr + i));
     }
     printf("\n");
