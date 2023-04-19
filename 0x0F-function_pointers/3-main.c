e56203ea25735a3601fe4d733c7effef1cf4d4c1
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-19 15:54:51 +0000

    dk

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+
+int main(int argc, argv[])
+{
+int a, b;
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+if (argv[2] == '+')
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "function_pointers.h"
+
+/**
+ * op_add - is a funtion
+ * @a: is an integer
+ * @b: is an int
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - is a funtion
+ * @a: is an integer
+ * @b: is an int
+ * Return: int
+ */
+int op_sub(int a, int b);
+{
+return (a - b);
+}
+
+/**
+ * op_mul - is a funtion
+ * @a: is an integer
+ * @b: is an int
+ * Return: int
+ */
+int op_mul(int a, int b);
+{
+return (a * b);
+}
+
+/**
+ * op_div - is a funtion
+ * @a: is an integer
+ * @b: is an int
+ * Return: int
+ */
+int op_div(int a, int b);
+{
+return (a / b);
+}
+
+/**
+ * op_mod - is a funtion
+ * @a: is an integer
+ * @b: is an int
+ * Return: int
+ */
+int op_mod(int a, int b);
+{
+return (a % b);
+}
