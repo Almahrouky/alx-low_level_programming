f9611ae84108870f0bf383185daef10e3c0b26e9
Almahrouky <ahmed.mahmoud717@el-eng.menofia.edu.eg>
2023-04-27 22:33:29 +0000

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,13 @@
 section .data
-	msg: db"Hello, Holberton",0xa
-	len: equ $ -msg
+msg: db"Hello, Holberton",0xa
+len: equ $ -msg
 section .text
-	global main
-	extern printf
+global main
+extern printf
 main:
-	mov rdi, msg
-	mov rax, 0
-	call printf
-	mov rax, 60
-	mov rdi, 0
-	syscall
+mov rdi, msg
+mov rax, 0
+call printf
+mov rax, 60
+mov rdi, 0
+syscall
